630369d56421a29648fbfdaef2a7b05d46c8bc26
geoffreymuchai <geoffreymuchai@gmail.com>
2013-04-19 17:20:48 +0300

    Rolled up version for release of 1.4-frontlinesms

--- StripeGrailsPlugin.groovy
+++ StripeGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class StripeGrailsPlugin {
-    def version = "1.4-frontlinesms-SNAPSHOT"
+    def version = "1.4-frontlinesms"
     def grailsVersion = "1.3.7 > *"
     def dependsOn = ['resources':'1.1.6 > *']
 
